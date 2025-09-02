2770f48da0ee9e8d24c9ff2c40cec8abf88ca3df
TomiTJ <tomitjnguyen@gmail.com>
2025-09-02 17:13:01 +1000

    front and backend - frontend structure and backend added enums

--- src/main/java/com/asd/model/Audit.java
+++ src/main/java/com/asd/model/Audit.java
@@ -1,9 +1,44 @@
 package com.asd.model;
 import jakarta.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.UUID;
 /* Notes:
  - Audit
 
  */
 
-public class Audit {
+enum Action {
+    CREATE, UPDATE, DELETE, APPROVE, REJECT, RESET_PASSWORD, APPROVE_PASSWORD, REJECT_PASSWORD
 }
+
+enum ResourceType {
+    User, Customer, Account, Transaction
+}
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "audit_event")
+public class Audit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "audit_event_id", nullable = false , updatable = false)
+    private java.util.UUID auditEventId;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "action" , nullable = false, length = 40)
+    private aciton action;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "resource_type" , nullable = false, length = 40)
+    private transactionType action;
+
+
+
+
+}
+
+
