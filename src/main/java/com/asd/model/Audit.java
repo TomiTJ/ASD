a8e9cc471f19023736d97ceeb6c26b60587bcd34
TomiTJ <tomitjnguyen@gmail.com>
2025-09-02 17:42:24 +1000

    Completed Draft for Audit model

--- src/main/java/com/asd/model/Audit.java
+++ src/main/java/com/asd/model/Audit.java
@@ -2,7 +2,9 @@ package com.asd.model;
 import jakarta.persistence.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
+import java.time.Instant;
 /* Notes:
  - Audit
 
@@ -20,22 +22,63 @@ enum ResourceType {
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "audit_event")
 public class Audit {
-
+    //which interaction (was it interaction 1, interaction 2. can be hundreds of interactions to be located from)
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "audit_event_id", nullable = false , updatable = false)
-    private java.util.UUID auditEventId;
+    private UUID auditEventId;
 
+    //who did (which user_id did it)
+    @Column(name = "actor_user_id", nullable = false , updatable = false)
+    private UUID userId;
 
+    //what did (e.g. was the interaction create , delete, approving or rejecting etc)
     @Enumerated(EnumType.STRING)
     @Column(name = "action" , nullable = false, length = 40)
-    private aciton action;
+    private Action action;
 
-
+    //on what (e.g. Customer, User , Account, Transaction)
     @Enumerated(EnumType.STRING)
     @Column(name = "resource_type" , nullable = false, length = 40)
-    private transactionType action;
+    private  ResourceType resourceType;
 
+    //on what specifically (e.g. which customer did it affect)
+    @Column(name = "resource_id", nullable = false, length = 40)
+    private  UUID resourceId;
+
+    //Track n Trace
+
+    @Column(name = "request_id",nullable = false)
+    private  UUID requestId;
+
+    //when did it happen
+
+    @org.springframework.data.annotation.CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+
+
+    public UUID getAuditEventId() {return auditEventId;}
+    public void setAuditEventId(UUID auditEventId) {this.auditEventId = auditEventId;}
+
+    public UUID getUserId() {return userId;}
+    public void setUserId(UUID userId) {this.userId = userId;}
+
+    public Action getAction() {return action;}
+    public void setAction(Action action) {this.action = action;}
+
+    public ResourceType getResourceType() {return resourceType;}
+    public void setResourceType(ResourceType resourceType) {this.resourceType = resourceType;}
+
+    public UUID getResourceId() {return resourceId;}
+    public void setResourceId(UUID resourceId) {this.resourceId = resourceId;}
+
+    public UUID getRequestId() {return requestId;}
+    public void setRequestId(UUID requestId) {this.requestId = requestId;}
+
+    public Instant getCreatedAt() {return createdAt;}
+    public void setCreatedAt(Instant createdAt) {this.createdAt = createdAt;}
 
 
 
