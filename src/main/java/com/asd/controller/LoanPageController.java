cbf55e47a4859406b69ebc2d9880535660219621
JoelOrm <160985234+JoelOrm@users.noreply.github.com>
2025-09-04 16:23:53 +1000

    renamed controller page to loandemopage

--- src/main/java/com/asd/controller/LoanPageController.java
+++ src/main/java/com/asd/controller/LoanPageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PageController {
+public class LoanPageController {
     @GetMapping({"/loan", "/loan.html"})
     public String loan() {
         return "loan"; // resolves templates/loan.html
