0a7dab06bae59aaa825f052a967bc245fbd18b8e
TomiTJ <tomitjnguyen@gmail.com>
2025-10-01 22:22:40 +1000

    added @Column to be consistent with SQL

--- src/main/java/com/asd/model/User.java
+++ src/main/java/com/asd/model/User.java
@@ -1,25 +1,29 @@
 package com.asd.model;
 
 import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String username;
-    private String password;
+    @Column(name="full_name")
+    private String full_name;
+    @Column(name="email",nullable=false)
     private String email;
+    @Column(name="password", nullable = false)
+    private String password;
+    @Column(name="role")
+    private String role;
+    @Column(name="status")
+    private String status;
+    @Column(name="created_at")
+    private LocalDateTime created_at;
+    @Column(name="updated_at")
+    private LocalDateTime updated_at;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable (
-            name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
-    private Set<Role> roles;
-    private boolean active;
-
-}
+  }
