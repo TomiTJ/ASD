ab9c63d90a0511d5d56ab7e9ffd723cbfd108c6b
TomiTJ <tomitjnguyen@gmail.com>
2025-10-01 12:51:33 +1000

    automatically fixed time creation upon new data insertion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,6 +25,36 @@ INSERT INTO transactions (customer_id, amount) VALUES (2, 2300);
 INSERT INTO transactions (customer_id, amount) VALUES (2, 30);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 DROP TABLE IF EXISTS audit_event;
 
 CREATE TABLE audit_event (
@@ -51,41 +81,6 @@ INSERT INTO audit_event (
              'Customer',
              'bbbb1111-bbbb-1111-bbbb-111111111111',
              'cccc1111-cccc-1111-cccc-111111111111',
-             '2025-09-03 08:00:00.000'
+          '2025-09-03 08:30:00'
          );
-
-INSERT INTO audit_event (
-    audit_event_id,
-    actor_user_id,
-    action,
-    resource_type,
-    resource_id,
-    request_id,
-    created_at
-) VALUES (
-             '22222222-2222-2222-2222-222222222222',
-             'aaaa2222-aaaa-2222-aaaa-222222222222',
-             'UPDATE',
-             'Account',
-             'bbbb2222-bbbb-2222-bbbb-222222222222',
-             'cccc2222-cccc-2222-cccc-222222222222',
-             '2025-09-03 09:15:00.000'
-         );
-
-INSERT INTO audit_event (
-    audit_event_id,
-    actor_user_id,
-    action,
-    resource_type,
-    resource_id,
-    request_id,
-    created_at
-) VALUES (
-             '33333333-3333-3333-3333-333333333333',
-             'aaaa3333-aaaa-3333-aaaa-333333333333',
-             'DELETE',
-             'Transaction',
-             'bbbb3333-bbbb-3333-bbbb-333333333333',
-             'cccc3333-cccc-3333-cccc-333333333333',
-             '2025-09-03 10:45:00.000'
-         );
+SELECT audit_event_id, created_at FROM audit_event;
