5743f503aa13a0e92c63d830669041a5159e755a
Lesandu Perera <lesandu2@gmail.com>
2025-10-02 14:38:19 +1000

    auth testv2 v2

--- src/test/java/com/asd/AuthControllerTests.java
+++ src/test/java/com/asd/AuthControllerTests.java
@@ -9,12 +9,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
+
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
