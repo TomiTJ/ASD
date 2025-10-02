108131d6ee834a817fe8b7295d547878b95e6253
TomiTJ <tomitjnguyen@gmail.com>
2025-10-02 15:51:17 +1000

    added dashboard demo and added 1 in front of dashboard mapping

--- src/main/java/com/asd/controller/DashboardPageController.java
+++ src/main/java/com/asd/controller/DashboardPageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DashboardPageController {
 
-    @GetMapping("/dashboard")
+    @GetMapping("/dashboard1")
     public String dashboardPage() {
         return "dashboard";
     }
